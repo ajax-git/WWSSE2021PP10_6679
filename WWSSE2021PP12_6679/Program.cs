39f01fe5dbd53428e444177edcfd9ffc79b1e835
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-15 13:45:33 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP12_6679/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Napisz program wypisujący w kolejnych wierszach wartości kolejnych liczb ciągu Fibonacciego mniejszych od zadanej wartości.
+*/
+
+int fib(int n)
+{
+	if (n < 3)
+		return 1;
+
+	return fib(n - 2) + fib(n - 1);
+}
+
+Console.Write("Podaj n= ");
+int number = int.Parse(Console.ReadLine()) - 1;
+
+for (int i = 1; i <= number; i++)
+{
+	Console.WriteLine("{0}", fib(i));
+}
